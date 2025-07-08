a031b85d75e5c9b26c80c892bacf4c0df07ea38d
MatchaEggTart <MatchaEggTart@hotmail.com>
2025-07-09 00:50:31 +0800

    updata

--- Chatbox/install.sh
+++ Chatbox/install.sh
@@ -1,10 +1,10 @@
-chmod +x $HOME/Downloads/Chatbox-1.14.3-x86_64.AppImage
+chmod +x $HOME/Downloads/Chatbox-1.15.0-x86_64.AppImage
 
 sudo mkdir -p /opt/google/chatbox/icons
 
 sudo cp $HOME/Workspace/Learn_Arch/Chatbox/chatbox.png /opt/google/chatbox/icons
 
-sudo cp $HOME/Downloads/Chatbox-1.14.3-x86_64.AppImage /opt/google/chatbox/
+sudo cp $HOME/Downloads/Chatbox-1.15.0-x86_64.AppImage /opt/google/chatbox/
 
 cp $HOME/Workspace/Learn_Arch/Chatbox/xyz.chatboxapp.app.desktop $HOME/.local/share/applications/xyz.chatboxapp.app.desktop 
 
