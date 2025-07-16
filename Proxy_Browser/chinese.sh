cb992b64a2088b3ef04aa0e0e4327f4da26b2678
MatchaEggTart <MatchaEggTart@hotmail.com>
2025-07-16 19:25:58 +0800

    update

---
+++ Proxy_Browser/chinese.sh
@@ -0,0 +1,6 @@
+# Exec= sh -c "export LANGUAGE=ZH-CN.UTF-8 && /usr/bin/google-chrome-stable %U"
+# Exec= sh -c "export LANGUAGE=ZH-CN.UTF-8 && /usr/bin/microsoft-edge-beta %U"
+
+/usr/bin/google-chrome-stable
+/usr/bin/microsoft-edge-beta 
+export LANGUAGE=ZH-CN.UTF-8
