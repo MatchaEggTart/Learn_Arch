c416b517297d015194ba57f0632ece49321f5470
MatchaEggTart <MatchaEggTart@hotmail.com>
2025-08-29 13:10:49 +0800

    Update

--- 01_Mirror/01_github520.sh
+++ 01_Mirror/01_github520.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-PATH_V="/etc/hostsBackup"
+PATH_V="/etc/hosts.backup"
 
 # 检查文件是否存在
 if [ -e "$PATH_V" ]; then
     echo "文件已存在"
 else
-    sudo cp /etc/hosts /etc/hostsBackup
+    sudo cp /etc/hosts /etc/hosts.backup
     echo "文件不存在"    
 fi
 
