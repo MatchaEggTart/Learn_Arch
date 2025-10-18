66e25417dd0d5506670c969dc1eacf4834f37aab
MatchaEggTart <MatchaEggTart@hotmail.com>
2025-10-19 02:16:15 +0800

    Update

--- 13_Tools/96_fastfetch.sh
+++ 13_Tools/96_fastfetch.sh
@@ -3,5 +3,6 @@ sudo pacman -S fastfetch lolcat
 # neofetch | lolcat
 # alias nfl="neofetch --source ~/.config/neofetch/ascii-art-neofetch/communist | lolcat"
 # alias nfll="neofetch --source ~/.config/neofetch/ascii-art-neofetch/communist -L | lolcat"
-cd ~/.local/share
-git clone https://github.com/LierB/fastfetch
+
+# cd ~/.local/share
+# git clone https://github.com/LierB/fastfetch
