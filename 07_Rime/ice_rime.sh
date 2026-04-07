a179ccd4582ee6e282c1da6eb1401e5d2dd126af
MatchaEggTart <MatchaEggTart@hotmail.com>
2026-04-07 22:59:21 +0800

    Update

--- 07_Rime/ice_rime.sh
+++ 07_Rime/ice_rime.sh
@@ -6,27 +6,8 @@ sudo pacman -S fcitx5 fcitx5-im fcitx5-rime
 paru -S rime-ice-git
 
 # Fcitx5
-# vim /etc/profile
-# export GTK_IM_MODULE=fcitx5
-# export QT_IM_MODULE=fcitx5
-# export XMODIFIERS=@im=fcitx
-# System Settings => Workspace Behavior => Virtual Desktop => fcitx5
-# https://fcitx-im.org/wiki/Using_Fcitx_5_on_Wayland
-# export XMODIFIERS=@im=fcitx
-
-# echo -e "# Fcitx 5" | sudo tee -a $PATH_PROFILE
-# echo -e "export XMODIFIERS=@im=fcitx" | sudo tee -a $PATH_PROFILE
-
-# KDE 是 写入 export XMODIFIERS=@im=fcitx 到 /etc/profile ?
-# Hyprland 是 写入 XMODIFIERS=@im=fcitx 到 /etc/environment
-
-# GTK_IM_MODULE=fcitx
+# Hyprland、KDE 是 写入 XMODIFIERS=@im=fcitx 到 /etc/environment
 
 # # Fcitx5
 # XMODIFIERS=@im=fcitx
-# # SDL_IM_MODULE=fcitx
-# # GLFW_IM_MODULE=ibus  # 注意：Kitty有时需要这个来触发输入框架
-
-# ~/.config/gtk-3.0/settings.ini
-# [Settings]
-# gtk-im-module = fcitx
+# GLFW_IM_MODULE=ibus  # 注意：Kitty有时需要这个来触发输入框架
--- 07_Rime/luna_rime.sh
+++ 07_Rime/luna_rime.sh
@@ -7,16 +7,8 @@ mkdir ~/.local/share/fcitx5/rime/
 sudo pacman -S fcitx5 fcitx5-im fcitx5-chinese-addons fcitx5-rime rime-luna-pinyin rime-emoji
 
 # Fcitx5
-# vim /etc/profile
-# export GTK_IM_MODULE=fcitx5
-# export QT_IM_MODULE=fcitx5
-# export XMODIFIERS=@im=fcitx
-# System Settings => Workspace Behavior => Virtual Desktop => fcitx5
-# https://fcitx-im.org/wiki/Using_Fcitx_5_on_Wayland
-# export XMODIFIERS=@im=fcitx
+# Hyprland、KDE 是 写入 XMODIFIERS=@im=fcitx 到 /etc/environment
 
-# echo -e "# Fcitx 5" | sudo tee -a $PATH_PROFILE
-# echo -e "export XMODIFIERS=@im=fcitx" | sudo tee -a $PATH_PROFILE
-
-# KDE 是 写入 export XMODIFIERS=@im=fcitx 到 /etc/profile ?
-# Hyprland 是 写入 XMODIFIERS=@im=fcitx 到 /etc/environment
+# # Fcitx5
+# XMODIFIERS=@im=fcitx
+# GLFW_IM_MODULE=ibus  # 注意：Kitty有时需要这个来触发输入框架
