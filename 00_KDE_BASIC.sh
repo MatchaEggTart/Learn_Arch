744bb82ead67e4122954f5c32c251be4945f9d67
MatchaEggTart <MatchaEggTart@hotmail.com>
2026-04-07 12:29:41 +0800

    Update

--- 00_KDE_BASIC.sh
+++ 00_KDE_BASIC.sh
@@ -1,4 +1,5 @@
 sudo pacman -S xdg-desktop-portal-kde xdg-desktop-portal
-sudo pacman  -S nvidia-utils
-sudo pacman -S kscreen bluez bluez-utils bluedevil plasma-meta
+#sudo pacman  -S nvidia-utils
+#sudo pacman -S kscreen bluez bluez-utils bluedevil plasma-meta
+sudo pacman -S plasma-meta
 
