4fb506526fc8d3887239c02c3aba239356f0b33b
MatchaEggTart <MatchaEggTart@hotmail.com>
2026-05-02 23:56:31 +0800

    Update

--- 09_Browser/chinese.sh
+++
@@ -1,6 +0,0 @@
-# Exec= sh -c "export LANGUAGE=ZH-CN.UTF-8 && /usr/bin/google-chrome-stable %U"
-# Exec= sh -c "export LANGUAGE=ZH-CN.UTF-8 && /usr/bin/microsoft-edge-beta %U"
-
-/usr/bin/google-chrome-stable
-/usr/bin/microsoft-edge-beta 
-export LANGUAGE=ZH-CN.UTF-8
--- 16_Opencode/opencode.sh
+++ 16_Opencode/opencode.sh
@@ -1,1 +1,9 @@
+sudo pacman -Syy bun
+
 paru -S opencode openspec
+
+# https://github.com/opensoft/oh-my-opencode/blob/dev/README.zh-cn.md
+
+cd $HOME/.config/opencode/
+
+opencode
