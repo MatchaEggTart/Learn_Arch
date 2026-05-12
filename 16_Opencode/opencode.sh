7c910cfd2155065033394aec86f91e5750e7ccba
MatchaEggTart <MatchaEggTart@hotmail.com>
2026-05-12 19:35:58 +0800

    Update

--- 16_Opencode/opencode.sh
+++ 16_Opencode/opencode.sh
@@ -1,6 +1,9 @@
 sudo pacman -Syy bun
+sudo pacman -S --needed github-cli
 
 paru -S opencode openspec
+
+# openspec init --tools opencode
 
 # https://github.com/opensoft/oh-my-opencode/blob/dev/README.zh-cn.md
 
